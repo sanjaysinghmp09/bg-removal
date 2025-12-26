c30c7bc7a9e5131e0983bdc8871b1b6ea11f1052
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-26 22:27:11 +0530

    header section completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,22 +8,22 @@ function Header() {
       <div >
             <h1 className='text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-700 leading-tight'>
               
-              Remove the <br /> <span className='bg-linear-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>background</span> from <br /> images for free 
+              Remove the <br className='max-md:hidden' /> <span className='bg-linear-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>background</span> from <br className='max-md:hidden' /> images for free 
             
             </h1>
-            <p>Remove the background from your images <br /> with our free online tool</p>
+            <p className='my-6 text-[15px] text-gray-600'>Remove the background from your images <br className='max-md:hidden' /> with our free online tool</p>
             <div>
               <input type="file" name='' id='upload1' hidden />
-              <label htmlFor="upload1">
-                <img src= {assets.upload_btn_icon} alt="upload-icon" />
-                <p>Upload your image</p>
+              <label className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-linear-to-r from-violet-500 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-500' htmlFor="upload1">
+                <img width={20} src= {assets.upload_btn_icon} alt="upload-icon" />
+                <p className='text-white text-sm'>Upload your image</p>
               </label>
             </div>
       </div>
 
       {/* right side  */}
-      <div>
-
+      <div className='w-full max-w-md'>
+        <img src={assets.header_img} alt="" />
       </div>
     </div>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,9 +9,60 @@ const Navbar = () => {
         <Link to='/'><img className='w-32 sm:w-44' src= {assets.logo} alt="logo" /></Link>
 
         
-        <button className='bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full'>
-            Get Started <img className='w-3 sm:w-4' src= {assets.arrow_icon} alt="arrow-icon" />
-        </button>
+        <button
+  className="
+    group
+    relative
+    overflow-hidden
+    bg-zinc-900
+    text-white
+    flex
+    items-center
+    gap-3
+    px-5 py-2.5
+    sm:px-8 sm:py-3
+    text-sm
+    rounded-full
+    cursor-pointer
+    transition-all
+    duration-500
+    ease-out
+    hover:scale-105
+    shadow-lg
+    hover:shadow-fuchsia-500/40
+  "
+>
+  {/* MAGIC LIGHT EFFECT */}
+  <span
+    className="
+      absolute
+      inset-0
+      bg-linear-to-r
+      from-transparent
+      via-fuchsia-500/20
+      to-transparent
+      translate-x-full
+      group-hover:translate-x-full
+      transition-transform
+      duration-700
+      ease-out
+    "
+  />
+
+  {/* TEXT */}
+  <span className="relative z-10 transition-transform duration-300 group-hover:-translate-x-1">
+    Get Started
+  </span>
+
+  {/* ARROW */}
+  <img
+    src={assets.arrow_icon}
+    alt="arrow-icon"
+    className="relative z-10 w-3 sm:w-4 opacity-0 translate-x-1.5 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-0
+    "
+  />
+</button>
+
     </div>
   )
 }
