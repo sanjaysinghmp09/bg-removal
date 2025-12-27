e7af40d4b74ed4671d269c43b7451797357fa5c7
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-27 10:32:50 +0530

    Steps jsx bug fixed

---
+++ client/src/components/Steps.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Steps = () => {
+  return (
+    <div><h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis molestiae, voluptatum possimus nam, aliquid labore explicabo aperiam maiores ipsam commodi et culpa corrupti.</h1></div>
+  )
+}
+
+export default Steps
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 function Home() {
   return (
-    <Header/>
+    <>
+      <Header />
+      <Steps />
+    </>
   )
 }
 
