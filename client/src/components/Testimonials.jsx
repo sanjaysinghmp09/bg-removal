65b64f3e1bb43a36e46d51084b36066e38023c56
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-28 20:31:58 +0530

    Customer Testimonials title are completed

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -4,7 +4,7 @@ function Testimonials() {
   return (
     <div>
         {/* Title */}
-        <h1 className='mb-12 sm:mb-20 text-center text-2xl md:text-3xl lg:text-4xl mt-1 font-semibold bg-linear-to-r from-gray-900 to-gray-500 bg-clip-text text-transparent '>What Our Customers Say <br /> About Us </h1>
+        <h1 className='mb-12 sm:mb-20 text-center text-2xl md:text-3xl lg:text-4xl mt-1 font-semibold bg-linear-to-r from-gray-900 to-gray-500 bg-clip-text text-transparent py-5'>What Our Customers Say <br /> About Us </h1>
 
     </div>
   )
