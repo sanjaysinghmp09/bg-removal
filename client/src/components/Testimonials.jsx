a4c4041c3f3846a10d2ca91ab8ee943fee228535
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-29 20:12:23 +0530

    Change font

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -5,18 +5,18 @@ function Testimonials() {
   return (
     <div>
         {/* Title */}
-        <h1 className='mb-12 sm:mb-20 text-center text-2xl md:text-3xl lg:text-4xl mt-1 font-semibold bg-linear-to-r from-gray-900 to-gray-500 bg-clip-text text-transparent py-5'>What Our Customers Say <br /> About Us </h1>
+        <h1 className='mb-10 sm:mb-12 text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-linear-to-r from-gray-900 to-gray-500 bg-clip-text text-transparent py-2'>What Our Customers Say <br /> About Us </h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto px-4 py-8'>
             {testimonialsData.map((item, index) => (
-                <div key={index} className='bg-white rounded-xl p-6 drop-shadow-md'>
-                    <p className='text-gray-700 italic mb-4'> "</p>
-                    <p className='text-gray-800 text-lg mb-4'> {item.text} </p>
+                <div key={index} className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-500'>
+                    <p className='text-4xl text-gray-500'> ”</p>
+                    <p className='text-sm text-gray-500'>{item.text}</p>
                     
-                    <div>
-                        <img src={item.image} alt="itemImage" />
+                    <div className='flex items-center gap-3 mt-5'>
+                        <img className='w-9 rounded-full' src={item.image} alt="itemImage" />
                         <div>
                             <p>{item.author}</p>
-                            <p>{item.jobTitle}</p>
+                            <p className='text-sm text-gray-600'>{item.jobTitle}</p>
 
                         </div>
                     </div>
