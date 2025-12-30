16cf93ecf29bf725cd1a97dff403a20c670febcb
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-30 18:05:02 +0530

    Add Footer jsx in App component for visiable all pages

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-        
+        <img src={assets.logo} alt="footerLogo" />
+        <p>Copyright @sanjaysingh | All rights reserved. </p>
     </div>
   )
 }
