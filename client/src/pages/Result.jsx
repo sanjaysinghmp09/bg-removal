dd9b9dc9041b3437b65b12cc98bb32dfc564b6a4
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-31 23:15:39 +0530

    create result pages two sides left and right

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 function Result() {
   return (
-    <div>
-      
+    <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
+      <div>
+        {/* ..........Image Container.......... */}
+        <div>
+          {/* Left sidwe image container */}
+
+          <div>
+            <p>Original</p>
+            <img src={assets.image_w_bg} alt="" />
+          </div>
+
+          {/* Right side image container */}
+          <div>
+            <p>Edited</p>
+            <img src={assets.image_wo_bg} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
