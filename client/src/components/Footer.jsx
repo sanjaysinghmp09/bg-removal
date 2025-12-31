0b8c746594992900d2def2e30dc6839bce581075
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-31 23:07:26 +0530

    Create Result Page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='flex items-center justify-between gap-4 px-4 lg:px-44 py-3'>
         <img width={150} src={assets.logo} alt="footerLogo" />
         <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @sanjaysingh | All rights reserved. </p>
-        <div className='flex gap-1'>
+        <div className='flex gap-1 cursor-pointer'>
           <img width={40} src={assets.facebook_icon} alt="" />
           <img width={40} src={assets.twitter_icon} alt="" />
           <img width={40} src={assets.google_plus_icon} alt="" />
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Result() {
   return (
-    <div>Result</div>
+    <div>
+      
+    </div>
   )
 }
 
