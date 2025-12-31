6caa5ed594760c7a1a0609156a15edd1865a6ee1
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-31 23:37:28 +0530

    add tailwind classes in result div

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -6,12 +6,12 @@ function Result() {
     <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
       <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm'>
         {/* ..........Image Container.......... */}
-        <div>
+        <div className='flex flex-col sm:grid grid-cols-2 gap-8'>
           {/* Left sidwe image container */}
 
           <div>
-            <p>Original</p>
-            <img src={assets.image_w_bg} alt="" />
+            <p className='font-semibold text-gray-600 mb-2'>Original</p>
+            <img className='rounded-md border border-gray-200' src={assets.image_w_bg} alt="" />
           </div>
 
           {/* Right side image container */}
