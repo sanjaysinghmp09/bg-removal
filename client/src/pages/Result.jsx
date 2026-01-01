77cc9c78b9691ca57eee904b1d07cdf40c9984bd
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-01 15:40:13 +0530

    add tailwind classes in result div

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -16,9 +16,29 @@ function Result() {
 
           {/* Right side image container */}
           <div className='flex flex-col'>
-            <p>Background Removed</p>
+            <p className='font-semibold text-gray-600 mb-2'>Background Removed</p>
+            <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
+
             <img src={assets.image_wo_bg} alt="" />
+
+            <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
+
+            {/* loader */}
+            {/* <div className='border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin' >
+            </div> */}
+
+            </div>
+            </div>
+
+          {/* .............Button...... */}
+
+          <div className='flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6'>
+            <button className='px-8 py-2.5 text-sm text-violet-600 border border-violet-600 rounded-full hover:scale-105 transition-all duration-700 '>Try another image</button>
+            <a className='px-8 py-2.5 text-white text-sm ' href="">Download Image</a>
           </div>
+
+          </div>
+          
         </div>
       </div>
     </div>
