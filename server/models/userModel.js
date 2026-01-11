2ae8a7f67c2e193715ebf7735f5b9530fdb72922
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-11 16:06:26 +0530

    schema design

---
+++ server/models/userModel.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    clerkId : { type:String , required:true , unique:true} ,
+    email : { type:String , required:true , unique:true}, 
+    email : { type:String , required:true , unique:true}, 
+})
