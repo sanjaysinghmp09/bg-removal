c65e59d44bdd216b7c3934286a16eaa458cd871c
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-11 15:49:10 +0530

    just test github repo working

--- server/server.js
+++ server/server.js
@@ -7,3 +7,15 @@ import cors from 'cors'
 
 const PORT = process.env.PORT || 4000
 const app = express()
+
+// Intialize Middleware
+
+app.use(express.json())
+app.use(cors())
+
+// API routes api 
+
+// get method 
+
+app.get('/' , (req , res)=>res.send("API Working"))
+app.listen(PORT , ()=> console.log('Server is running on port' + PORT))
