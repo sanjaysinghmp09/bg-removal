d38cd1855aa77cc27bc3e1e32eb38bac81093968
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-11 16:02:13 +0530

    mongoDB connected !

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectDB from './configs/mongodb.js'
 
 
 // App Config 
 
 const PORT = process.env.PORT || 4000
 const app = express()
+await connectDB  ()
 
 // Intialize Middleware
 
