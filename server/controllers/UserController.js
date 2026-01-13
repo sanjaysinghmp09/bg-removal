f4295bd940a82263a616eed1f0148ecf2026dc52
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-13 10:03:36 +0530

    mongoDB related problem solved

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,3 +2,9 @@
 
 // API CONTROLLER function to manage clerk User with database
 // http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async(req , res) => {
+    try{
+
+    } catch(err)
+}
