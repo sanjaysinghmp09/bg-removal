43a9d215e447a58085bada324cc09f441851b080
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-13 19:21:12 +0530

    svix add and id with param

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,4 @@
-
+import {Webhook} from 'svix'
 
 // API CONTROLLER function to manage clerk User with database
 // http://localhost:4000/api/user/webhooks
@@ -6,5 +6,14 @@
 const clerkWebhooks = async(req , res) => {
     try{
 
-    } catch(err)
+        // create a svix instance with clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await whook.verify(JSON.stringify(req.body){
+            "svix-id" :req.headers
+        })
+
+    } catch(error){
+
+    }
 }
