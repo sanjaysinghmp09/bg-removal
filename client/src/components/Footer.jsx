7c7ccc244b1a6398b24bc241933f67c1af55ec8b
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-14 20:02:23 +0530

    add comment in footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
+// footer section
+
 const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 px-4 lg:px-44 py-3'>
