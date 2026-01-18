0c0a6101da7578f7dc54c83a8496d9bae2a146bd
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-18 20:34:51 +0530

    error on the way

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,15 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    mongoose.connection.on("connected", () => {
-      console.log("Database Connected");
-    });
+  mongoose.connection.on("connected", () => {
+    console.log("Database Connected");
+  });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/remove-bg`);
-  } catch (error) {
-    console.log("MongoDB Connection Error:", error.message);
-  }
+  await mongoose.connect(`${process.env.MONGODB_URI}/remove-bg`);
 };
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,22 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import connectDB from './configs/mongodb.js'
-import userRouter from './routes/userRoutes.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/mongodb.js";
+import useRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imagesRoutes.js";
 
+// App Config
+const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
 
-// App Config 
+// Initialized Middlewares
+app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 4000
-const app = express()
-await connectDB()
+// API Routes
+app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/user", useRouter);
+app.use("/api/image", imageRouter);
 
-// Intialize Middleware
-
-app.use(express.json())
-app.use(cors())
-
-// API routes api  
-
-app.get('/' , (req , res)=>res.send("API Working"))
-
-app.use('/api/user' , userRouter)
-app.listen(PORT , ()=> console.log('Server is running on port' + PORT))
+app.listen(PORT, () => console.log("Server running on port: ", PORT));
