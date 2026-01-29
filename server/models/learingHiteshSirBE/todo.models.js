3fb217d282c36d969b7f7efc6748f19f7f00c559
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-01-29 20:23:47 +0530

    write cmt

--- server/models/learingHiteshSirBE/todo.models.js
+++ server/models/learingHiteshSirBE/todo.models.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 
 const User = new mongoose.Schema({
     
+
 } , {timestamps : true})
 
 export const members = mongoose.model("members" , User )
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {clerkWebhooks} from '../controllers/UserController.js'
 
 const userRouter = express.Router()
 
+
 userRouter.post('/webhooks' , clerkWebhooks)
 
 export default userRouter
